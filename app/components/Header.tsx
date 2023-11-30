9143845efe096eab2b6d162f07fe910040287dc6
ahmedElsehrawy <aelsehrawy1999@gmail.com>
2023-11-30 04:09:31 +0200

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+const Header = () => {
+  return <HeaderContainer>Header</HeaderContainer>;
+};
+
+const HeaderContainer = styled.div`
+  background-color: red;
+`;
+
+export default Header;
